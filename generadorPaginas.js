00cf9b4eac02bcc6f431675f7b09b73dcc5db6f9
Andrés Martin Mena <andres.martin.m@ug.uchile.cl>
2026-05-14 23:56:11 -0400

    nuevos elementos

---
+++ generadorPaginas.js
@@ -0,0 +1,30 @@
+async function proyecto() {
+    var numero = new URLSearchParams(window.location.search).get("nro");
+    numero = Number(numero);
+    const response = await fetch("proyectos.json");
+    const proyectos = await response.json();
+
+    // carrusel imagenes del objeto completo y en uso
+
+    proyectos[numero].imagenesCompleto.forEach((x) => {
+        document.querySelector("#carouselObj-ind").innerHTML += `
+        <li data-bs-target="#carouselObj" data-bs-slide-to="` + x + `" class="active" aria-current="true" aria-label="Slide element"></li>`;
+    });
+    proyectos[numero].imagenesCompleto.forEach((x) => {
+        if (x == proyectos[numero].imagenesCompleto[0]) {
+            document.querySelector("#carouselObj-inn").innerHTML += `
+            <div class="carousel-item active"><img src="` + x + `" class="w-100 d-block" alt="Slide element"/></div>`;
+        }
+        document.querySelector("#carouselObj-inn").innerHTML += `
+        <div class="carousel-item"><img src="` + x + `" class="w-100 d-block" alt="Slide element"/></div>`;
+    });
+
+
+    // titulo
+
+    // objeto 360
+
+    // texto explicativo
+
+    // carrusel de proceso y partes
+}
