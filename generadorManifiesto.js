555bd087374574c53ae0479d31287c4434e053d3
Andrés Martin Mena <andres.martin.m@ug.uchile.cl>
2026-05-17 14:29:03 -0400

    manifiesto markdown

--- generadorManifiesto.js
+++ generadorManifiesto.js
@@ -1,8 +1,8 @@
 async function proyecto() {
-    const response = await fetch("https://raw.githubusercontent.com/AndresMartinM/concalma-web/refs/heads/main/concalma-manifiesto.txt");
+    const response = await fetch("https://raw.githubusercontent.com/AndresMartinM/concalma-web/refs/heads/main/manifiesto.md");
     const texto = await response.text();
 
-    document.querySelector("p").innerHTML = texto;
+    document.querySelector("md-block").innerHTML = texto;
 
 }
 
