0eb1dc30ffaf6d5215a8afd848184eb1f0856e47
Andrés Martin Mena <andres.martin.m@ug.uchile.cl>
2026-05-17 13:43:36 -0400

    proyectos y style

---
+++ generadorProyectos.js
@@ -0,0 +1,35 @@
+async function proyectos() {
+
+    // cargar el json de proyectos
+    const response = await fetch("https://raw.githubusercontent.com/AndresMartinM/concalma-web/refs/heads/main/proyectos.json");
+    const data = await response.json();
+
+    // seleccionar el contenedor de los proyectos
+    const contenedor = document.querySelector("#proyectos");
+
+    // crear las tarjetas de los proyectos
+    data.forEach((x, i) => {
+        contenedor.innerHTML += `
+        <div class="col-md-4">
+            <a class="card" href="prototipo.html?nro=` + i + `" style="background-color: ` + x.colorFondo + `; color: ` + x.colorTexto + `;">
+                <img
+                    class="card-img-top"
+                    src="` + x.sneakPeek + `"
+                    alt="Card image cap"
+                />
+                <div class="card-body">
+                    <h4 class="card-title">` + x.titulo + `</h4>
+                    <p class="card-text">` + x.descripcion + `</p>
+                </div>
+            </a>
+        </div>
+        `;
+    });
+
+
+}
+
+// esta parte es importantisima
+proyectos().catch((error) => {
+    console.error("Error al cargar el proyecto:", error);
+});
