4e31c5f9fee69155c4dea278e227d7d6eba5936e
Andrés Martin Mena <andres.martin.m@ug.uchile.cl>
2026-05-17 21:06:24 -0400

    Update generadorManifiesto.js

--- generadorManifiesto.js
+++ generadorManifiesto.js
@@ -2,7 +2,7 @@ async function proyecto() {
     const response = await fetch("https://raw.githubusercontent.com/AndresMartinM/concalma-web/refs/heads/main/manifiesto.md");
     const texto = await response.text();
 
-    document.querySelector("md-block").innerHTML = texto;
+    //document.querySelector("md-block").innerHTML = texto;
 
 }
 
